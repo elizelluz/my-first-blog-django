474a547dc2de7af45a4ebb6222d44eb39f88971c
Eliz <elizelluz@gmail.com>
2018-01-19 21:19:05 -0400

    base de datos

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mysite_django',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #    'NAME': 'mysite_django',
+    #    'USER': 'postgres',
+    #    'PASSWORD': '123456',
+    #    'HOST': 'localhost',
+    #    'PORT': '5432',
+    #}
 }
 
 
